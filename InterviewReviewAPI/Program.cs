8e3af2edfbb969e8c956e00e3a074e6360d4717e
Vekkula <veli.jakkula@gmail.com>
2023-02-18 23:37:22 +0200

    enabled swagger in production

--- InterviewReviewAPI/Program.cs
+++ InterviewReviewAPI/Program.cs
@@ -16,9 +16,9 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSwagger();
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
     app.UseSwaggerUI();
 }
 
